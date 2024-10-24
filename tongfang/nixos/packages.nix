ecfb278d2a01674f97b9bc38b69850195f4a3979
Bart van der Braak <bart@vanderbraak.nl>
2024-10-24 18:01:40 +0200

    Add wallpaper from fetched URL

--- tongfang/nixos/packages.nix
+++ tongfang/nixos/packages.nix
@@ -1,7 +1,10 @@
 { pkgs, inputs, config, ... }:
 
 let
-  customWallpaper = "/nix/store/fvplr8bbp5b9n2x6s21bp4nai80adk8j-wallhaven-zyerpj.jpg";
+  customWallpaper = pkgs.fetchurl {
+    url = "https://w.wallhaven.cc/full/2y/wallhaven-2y2wg6.png";
+    sha256 = "9c5a0d7e4ed8fc218a5adb1c384e463b1b212397859a9a56be1c47cce27a9820";
+  };
 in
 {
   environment.systemPackages = with pkgs; with inputs; [
